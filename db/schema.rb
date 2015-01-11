f7cff75ec34799b6ffd15ccd7a8709159f2f7047
Travis Grathwell <tjgrathwell@gmail.com>
2015-01-10 21:36:24 -0800

    Update schema.rb to Rails 4.2 variant

--- db/schema.rb
+++ db/schema.rb
@@ -9,53 +9,53 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120330051402) do
+ActiveRecord::Schema.define(version: 20120330051402) do
 
-  create_table "categories", :force => true do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "category_preferences", :force => true do |t|
+  create_table "category_preferences", force: :cascade do |t|
     t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "identities", :force => true do |t|
+  create_table "identities", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "scores", :force => true do |t|
+  create_table "scores", force: :cascade do |t|
     t.integer  "wpm"
     t.float    "accuracy"
     t.integer  "snippet_id"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "snippets", :force => true do |t|
+  create_table "snippets", force: :cascade do |t|
     t.text     "full_text"
     t.string   "short_desc"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "category_id"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-    t.boolean  "is_admin",   :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "is_admin",   default: false
   end
 
 end
