f97c443fc2a4fcd47766fdc016b51b97fd2d9e6c
Travis Grathwell <tjgrathwell@gmail.com>
2015-04-23 23:00:12 -0400

    Destroying a category (if it were possible) destroys its snippets/prefs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-  has_many :snippets
-  has_many :category_preferences
+  has_many :snippets, dependent: :destroy
+  has_many :category_preferences, dependent: :destroy
 end
