aef072474c93ff7a9e3567b3097daf22c3494eb0
Travis Grathwell <tjgrathwell@gmail.com>
2016-04-04 23:25:15 -0700

    Render preferences modal using flex instead of absolute positioning
    This allows it to be used even when your dev inspector is gobbling
    up most of the screen.

--- frontend/app/components/prefs-popup-content.js
+++ frontend/app/components/prefs-popup-content.js
@@ -5,13 +5,5 @@ export default Ember.Component.extend({
 
   click: function (e) {
     e.stopPropagation();
-  },
-
-  didInsertElement: function () {
-    this._super();
-    this.$().css({
-      left: $('.container').offset().left + 40,
-      top: $(window).height() / 4
-    });
   }
 });
