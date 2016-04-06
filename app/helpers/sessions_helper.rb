9d70534fb5591a2d3dda23e55d024c1c4ac838f7
Travis Grathwell <tjgrathwell@gmail.com>
2016-04-05 22:47:11 -0700

    Add strategic colons to User reference to fix autoloading problem
    Without these, any change in Railsland made after server start gives this error:
    
    "SessionHelper has been removed from the module tree but is still active!"
    
    It's not clear why this is happening; internet people seem to think it has
    something to do with loading autoloaded files from non-autoloaded locations
    or vice-versa. But that doesn't seem to make sense because app/helpers and
    app/controllers should both be autoloaded. Who knows!

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
+    @current_user ||= ::User.find_by_id(session[:user_id])
   end
 
   def signed_in?
